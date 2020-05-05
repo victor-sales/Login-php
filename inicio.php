8458e1331c67035ed589f3603dad62b547604b9b
Victor Andrade Sales <victor.andrade.sales@gmail.com>
2020-05-05 18:03:22 -0300

    teste

--- inicio.php
+++ inicio.php
@@ -8,22 +8,34 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true
     header('location:index.php');
 }
 
-$logado = $_SESSION['email']
+
+$logado = $_SESSION['email'];
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Olá</title>
+    <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.4.1.js" integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU=" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="js/main.js"></script>
 </head>
 
+<nav class="navbar navbar-light bg-light">
+    <div class="container">
+        <p class="navbar-brand boas-vindas"><?php echo "Olá " . $logado; ?></p>
+        <button class="btn btn-danger"><a class="sair" href=""><?php session_destroy(); ?>Sair</a></button>
+    </div>
+</nav>
+
 <body>
-    <p><?php echo "Olá " . $logado; ?></p>
-    <a href="index.php"><?php session_destroy();?> Sair</a>
+
 </body>
 
 </html>
